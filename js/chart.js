134f69e61b4ef2b282ce68b29b1c216ac9db9754
Guilherme Silva <guilhermebaos@gmail.com>
2021-04-12 10:09:24 +0100

    Outra Poupança

--- js/chart.js
+++ js/chart.js
@@ -1,43 +1,45 @@
 const montanteInicial = 10000
 const IRS = 1 - 0.28
-let anos, capital, lucro
+let anos, capital, lucro, trimestes
+let ultimoCapital, ultimoLucro
 
-// POUPANÇA CTT
-const ChartPoupancaCtt = document.getElementById('ChartPoupancaCtt')
 
-let trimestes = [0]
+// POUPANÇA NovoBanco
+const ChartPoupancaNovoBanco = document.getElementById('ChartPoupancaNovoBanco')
+
+trimestes = [0]
 capital = [montanteInicial]
 lucro = [0]
 
-let taxaBase = 0.457 / 100
-let premio = 0, juroTotal = 0
+const TAEL = 0.744 / 100
 for (let t = 1; t <= 40; t++) {
     trimestes.push(t)
 
-    if (t > 4) premio = 0.5 / 100
-    if (t > 20) premio = 1 / 100
-
-    juroTotal = (taxaBase + premio) / 4
+    if (t % 4 != 0) {
+        capital.push(capital[capital.length - 1])
+        lucro.push(0)
+        continue
+    }
 
     ultimoCapital = capital[capital.length - 1]
-    ultimoLucro = ultimoCapital * juroTotal * IRS
+    ultimoLucro = ultimoCapital * TAEL
 
     capital.push(ultimoCapital + ultimoLucro)
     lucro.push(ultimoLucro)
 }
 
-new Chart(ChartPoupancaCtt, {
+new Chart(ChartPoupancaNovoBanco, {
     type: 'line',
     data: {
         labels: trimestes,
         datasets: [{
             data: capital,
-            label: 'Capital Total',
+            label: 'Capital',
             borderColor: 'blue',
             fill: false
         },{
             data: lucro,
-            label: 'Lucro em Cada Trimestre',
+            label: 'Lucro',
             borderColor: 'red',
             fill: false
         }]
@@ -56,7 +58,7 @@ new Chart(ChartPoupancaCtt, {
             yAxes: [{
                 scaleLabel: {
                     display: true,
-                    labelString: 'Capital Total (€)',
+                    labelString: 'Capital/ €',
                     fontColor: 'black',
                     fontSize: 13,
                     fontFamily: '"Arial", "sans-serif"'
@@ -86,6 +88,95 @@ new Chart(ChartPoupancaCtt, {
         }
     },
 })
+
+
+
+// POUPANÇA CTT
+const ChartPoupancaCtt = document.getElementById('ChartPoupancaCtt')
+
+trimestes = [0]
+capital = [montanteInicial]
+lucro = [0]
+
+let taxaBase = 0.457 / 100
+let premio = 0, juroTotal = 0
+for (let t = 1; t <= 40; t++) {
+    trimestes.push(t)
+
+    if (t > 4) premio = 0.5 / 100
+    if (t > 20) premio = 1 / 100
+
+    juroTotal = (taxaBase + premio) / 4
+
+    ultimoCapital = capital[capital.length - 1]
+    ultimoLucro = ultimoCapital * juroTotal * IRS
+
+    capital.push(ultimoCapital + ultimoLucro)
+    lucro.push(ultimoLucro)
+}
+
+new Chart(ChartPoupancaCtt, {
+    type: 'line',
+    data: {
+        labels: trimestes,
+        datasets: [{
+            data: capital,
+            label: 'Capital',
+            borderColor: 'blue',
+            fill: false
+        },{
+            data: lucro,
+            label: 'Lucro',
+            borderColor: 'red',
+            fill: false
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Trimestres de Poupança',
+                    fontColor: 'black',
+                    fontSize: 13,
+                    fontFamily: '"Arial", "sans-serif"'
+                }
+            }],
+            yAxes: [{
+                scaleLabel: {
+                    display: true,
+                    labelString: 'Capital/ €',
+                    fontColor: 'black',
+                    fontSize: 13,
+                    fontFamily: '"Arial", "sans-serif"'
+                },
+                ticks: {
+                    min: 0
+                }
+            }]
+        },
+        tooltips: {
+            callbacks: {
+                title: function(tooltipItems, data) {
+                    let tooltipItem = tooltipItems[0]
+
+                    return 'Trimestres de Poupança: ' + tooltipItem.label
+                },
+                label: function(tooltipItem, data) {
+                    let value = Number(tooltipItem.value).toFixed(2)
+
+                    return data.datasets[tooltipItem.datasetIndex].label + ': ' + value + '€'
+                }
+            },
+            custom: function(tooltip) {
+                if (!tooltip) return
+                tooltip.displayColors = false
+            },
+        }
+    },
+})
+
+
 
 
 // INVESTIMENTOS
@@ -184,10 +275,11 @@ new Chart(ChartInvestimentos, {
 })
 
 const capitalInicial = document.getElementById('capitalInicial')
-const reforcoAnual = document.getElementById('reforcoAnual')
 const retornoAnual = document.getElementById('retornoAnual')
+const tempo = document.getElementById('tempo')
+const modalidade = document.getElementById('modalidade')
 
-capital = []
+capital = [], lucro = []
 let graficoInv = new Chart(ChartInvestimentosComp, {
     type: 'line',
     data: {
@@ -195,6 +287,11 @@ let graficoInv = new Chart(ChartInvestimentosComp, {
         datasets: [{
             data: capital,
             label: 'Capital',
+            borderColor: 'blue',
+            fill: false
+        },{
+            data: lucro,
+            label: 'Lucro',
             borderColor: 'red',
             fill: false
         }]
@@ -213,13 +310,10 @@ let graficoInv = new Chart(ChartInvestimentosComp, {
             yAxes: [{
                 scaleLabel: {
                     display: true,
-                    labelString: 'Capital Total (€)',
+                    labelString: 'Capital/ €',
                     fontColor: 'black',
                     fontSize: 13,
                     fontFamily: '"Arial", "sans-serif"'
-                },
-                ticks: {
-                    min: 0
                 }
             }]
         },
@@ -233,7 +327,7 @@ let graficoInv = new Chart(ChartInvestimentosComp, {
                 label: function(tooltipItem, data) {
                     let value = Number(tooltipItem.value).toFixed(2)
 
-                    return 'Capital: ' + value + '€'
+                    return data.datasets[tooltipItem.datasetIndex].label + ': ' + value + '€'
                 }
             },
             custom: function(tooltip) {
@@ -245,18 +339,36 @@ let graficoInv = new Chart(ChartInvestimentosComp, {
 })
 function InvestimentosComp() {
     let cI = capitalInicial.value / 1
-    let rM = reforcoAnual.value / 1
     let r = retornoAnual.value / 100
+    let t = tempo.value / 1
+    let m = modalidade.value == 'Acumulação'
+
+    let IRSreal
+    if (m) IRSreal = 1.00
+    else IRSreal = IRS
     
     anos = [anoInicial]
-    let capital = [cI]
-    for (i = anoInicial + 1; i < anoMax; i++) {
+    capital = [cI]
+    lucro = [0]
+    for (i = anoInicial + 1; i < t + 1; i++) {
         anos.push(i)
+
+        ultimoCapital = capital[capital.length - 1]
+        ultimoLucro = ultimoCapital * r * IRSreal
     
-        capital.push(capital[capital.length - 1] * (1 + r) + rM)
+        capital.push(ultimoCapital + ultimoLucro)
+        lucro.push(ultimoLucro)
+    }
+    if (m) {
+        let imposto = lucro.reduce((a, b) => a + b) * (1 - IRS)
+
+        anos.push(i)
+        capital[capital.length - 1] -= imposto
+        lucro.push(-imposto)
     }
 
     graficoInv.data.labels = anos
     graficoInv.data.datasets[0].data = capital
+    graficoInv.data.datasets[1].data = lucro
     graficoInv.update()
 }
